00d7798722d2c309a44ba272047397a47c11eafa
kimango0o <kimango0o@outlook.kr>
2021-08-16 15:40:01 +0900

    레이아웃 잡기

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="index">
-    
+    <b-container class="bv-example-row">
+      <b-row class="text-center">
+        <b-col></b-col>
+        <b-col cols="8">
+          <b-card>
+            <h2 class="title">TODO LIST</h2>
+          <hr>
+            <b-nav tabs fill>
+              <b-nav-item active>
+                +
+              </b-nav-item>
+              
+              <b-nav-item>YEAR</b-nav-item>
+              <b-nav-item>MONTH</b-nav-item>
+              <b-nav-item>WEEK</b-nav-item>
+              <b-nav-item>TODAY</b-nav-item>
+            </b-nav>
+
+          <b-card>
+            
+          </b-card>
+          </b-card>
+            
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -11,5 +37,17 @@ export default {
 </script>
 
 <style>
+  body {
+    background-color: #F9F8ED;
+  }
 
+  #index {
+    margin-top: 10px;
+  }
+
+  .title {
+    color: #7A6462;
+    font-weight: bold;
+    text-shadow: 0px 1px #ffffff, 3px 3px 0px #dad7d7;
+  }
 </style>
