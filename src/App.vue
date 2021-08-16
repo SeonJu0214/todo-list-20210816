a9e6b648fb0b0a02702fb1451b8b0542db37e028
kimango0o <kimango0o@outlook.kr>
2021-08-17 01:07:36 +0900

    삭제 오류 해결~~

--- src/App.vue
+++ src/App.vue
@@ -74,7 +74,8 @@
       },
       removeTodo(index) {
         // 로컬 스토리지의 데이터를 삭제
-        localStorage.removeItem(index);
+        var key = localStorage.key(index);
+        localStorage.removeItem(key);
         /*
             splice() : 배열의 특정 인덱스를 삭제하는 API
             할 일 삭제 처리
